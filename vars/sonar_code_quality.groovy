d01a5a52df8f76a1cfae8c823c732f6ea129fba7
Nisharg Soni <142780556+Nisharg-04@users.noreply.github.com>
2024-12-29 17:46:06 +0530

    Update sonar_code_quality.groovy

--- vars/sonar_code_quality.groovy
+++ vars/sonar_code_quality.groovy
@@ -1,5 +1,5 @@
 def call() {
-    timeout(time: 1, unit: "MINUTES") {
+    timeout(time: 5, unit: "MINUTES") { // Increase the timeout as needed
         def qualityGate = waitForQualityGate()
         if (qualityGate.status != 'OK') {
             error "Pipeline aborted due to quality gate failure: ${qualityGate.status}"
